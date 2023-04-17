e457edcc3a2252e690a80d0cc27a338f6d2f6467
Cringuha <98592983+Cringuha@users.noreply.github.com>
2023-04-17 22:30:14 +0400

    Add files via upload

--- XII_15.cpp
+++ XII_15.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
+bool pred(int x){
+    return x > 0;
+}
+
 int main() {
-	vector <int> v = { 0,515,-124,220,23543,-12543 };
-	auto iter = max_element(v.begin(), v.end());
-	iter_swap(v.begin(), iter);
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i]<< '\t';
-	}
-	setlocale(0, " ");
-	return 0;
+    vector<int> v = { 5, -2, 8, 3, -1, -9 };
+    int newElem;
+
+    cout << "enter new element=";
+    cin >> newElem;
+    cout << endl;
+
+    auto it = find_if(v.rbegin(), v.rend(), pred).base();
+    v.insert(it, newElem);
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
